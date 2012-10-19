eda34bcd2b9466600262cad7f18b8a003438e372
Mark Heath <mark.heath@gmail.com>
2012-10-19 13:49:43 +0100

    initial commit

---
+++ MemoryLeakTest/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace MemoryLeakTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelShortLived = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.labelShortLivedEventSubscribers = new System.Windows.Forms.Label();
+            this.buttonForceGC = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.labelShortLivedPublishers = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.labelShortLivedSubscribers = new System.Windows.Forms.Label();
+            this.buttonShortLivedWeakSubscribers = new System.Windows.Forms.Button();
+            this.labelShortLivedWeakSubscribers = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Short-Lived Event Raisers";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OnSubscribeToShortlivedObjects);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.OnTimerTick);
+            // 
+            // labelShortLived
+            // 
+            this.labelShortLived.AutoSize = true;
+            this.labelShortLived.Location = new System.Drawing.Point(219, 18);
+            this.labelShortLived.Name = "labelShortLived";
+            this.labelShortLived.Size = new System.Drawing.Size(39, 13);
+            this.labelShortLived.TabIndex = 1;
+            this.labelShortLived.Text = "{code}";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(194, 24);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Short-Lived Event Subscribers";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.OnShortlivedEventSubscribersClick);
+            // 
+            // labelShortLivedEventSubscribers
+            // 
+            this.labelShortLivedEventSubscribers.AutoSize = true;
+            this.labelShortLivedEventSubscribers.Location = new System.Drawing.Point(219, 49);
+            this.labelShortLivedEventSubscribers.Name = "labelShortLivedEventSubscribers";
+            this.labelShortLivedEventSubscribers.Size = new System.Drawing.Size(39, 13);
+            this.labelShortLivedEventSubscribers.TabIndex = 1;
+            this.labelShortLivedEventSubscribers.Text = "{code}";
+            // 
+            // buttonForceGC
+            // 
+            this.buttonForceGC.Location = new System.Drawing.Point(13, 160);
+            this.buttonForceGC.Name = "buttonForceGC";
+            this.buttonForceGC.Size = new System.Drawing.Size(194, 23);
+            this.buttonForceGC.TabIndex = 3;
+            this.buttonForceGC.Text = "Force Garbage Collection";
+            this.buttonForceGC.UseVisualStyleBackColor = true;
+            this.buttonForceGC.Click += new System.EventHandler(this.OnForceGcClick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 73);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(194, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Short-Lived publishers";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.OnShortlivedEventPublishersClick);
+            // 
+            // labelShortLivedPublishers
+            // 
+            this.labelShortLivedPublishers.AutoSize = true;
+            this.labelShortLivedPublishers.Location = new System.Drawing.Point(219, 78);
+            this.labelShortLivedPublishers.Name = "labelShortLivedPublishers";
+            this.labelShortLivedPublishers.Size = new System.Drawing.Size(39, 13);
+            this.labelShortLivedPublishers.TabIndex = 1;
+            this.labelShortLivedPublishers.Text = "{code}";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 102);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(194, 23);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Short-Lived subscribers";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.OnShortLivedEventBusSubscribersClick);
+            // 
+            // labelShortLivedSubscribers
+            // 
+            this.labelShortLivedSubscribers.AutoSize = true;
+            this.labelShortLivedSubscribers.Location = new System.Drawing.Point(219, 107);
+            this.labelShortLivedSubscribers.Name = "labelShortLivedSubscribers";
+            this.labelShortLivedSubscribers.Size = new System.Drawing.Size(39, 13);
+            this.labelShortLivedSubscribers.TabIndex = 1;
+            this.labelShortLivedSubscribers.Text = "{code}";
+            // 
+            // buttonShortLivedWeakSubscribers
+            // 
+            this.buttonShortLivedWeakSubscribers.Location = new System.Drawing.Point(13, 131);
+            this.buttonShortLivedWeakSubscribers.Name = "buttonShortLivedWeakSubscribers";
+            this.buttonShortLivedWeakSubscribers.Size = new System.Drawing.Size(194, 23);
+            this.buttonShortLivedWeakSubscribers.TabIndex = 0;
+            this.buttonShortLivedWeakSubscribers.Text = "Short-Lived weak subscribers";
+            this.buttonShortLivedWeakSubscribers.UseVisualStyleBackColor = true;
+            this.buttonShortLivedWeakSubscribers.Click += new System.EventHandler(this.OnShortLivedWeakSubscribersClick);
+            // 
+            // labelShortLivedWeakSubscribers
+            // 
+            this.labelShortLivedWeakSubscribers.AutoSize = true;
+            this.labelShortLivedWeakSubscribers.Location = new System.Drawing.Point(219, 136);
+            this.labelShortLivedWeakSubscribers.Name = "labelShortLivedWeakSubscribers";
+            this.labelShortLivedWeakSubscribers.Size = new System.Drawing.Size(39, 13);
+            this.labelShortLivedWeakSubscribers.TabIndex = 1;
+            this.labelShortLivedWeakSubscribers.Text = "{code}";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 262);
+            this.Controls.Add(this.buttonForceGC);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.labelShortLivedEventSubscribers);
+            this.Controls.Add(this.labelShortLivedWeakSubscribers);
+            this.Controls.Add(this.labelShortLivedSubscribers);
+            this.Controls.Add(this.labelShortLivedPublishers);
+            this.Controls.Add(this.labelShortLived);
+            this.Controls.Add(this.buttonShortLivedWeakSubscribers);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelShortLived;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label labelShortLivedEventSubscribers;
+        private System.Windows.Forms.Button buttonForceGC;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label labelShortLivedPublishers;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label labelShortLivedSubscribers;
+        private System.Windows.Forms.Button buttonShortLivedWeakSubscribers;
+        private System.Windows.Forms.Label labelShortLivedWeakSubscribers;
+    }
+}
+
---
+++ MemoryLeakTest/Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reactive.Subjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MemoryLeakTest
+{
+    public partial class Form1 : Form
+    {
+        private int shortlivedEventRaiserCreated;
+        private int shortlivedEventSubscriberCreated;
+        private int shortlivedEventPublisherCreated;
+        private int shortlivedEventBusSubscriberCreated;
+        private int shortlivedWeakEventSubscriberCreated;
+        private readonly EventPublisher publisher1;
+        private readonly EventPublisher publisher2;
+        private readonly WeakEventAggregator weakEventAggregator;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 1000;
+            timer1.Start();
+            OnTimerTick(this, EventArgs.Empty);
+            publisher1 = new EventPublisher();
+            publisher2 = new EventPublisher();
+            weakEventAggregator = new WeakEventAggregator();
+        }
+
+        private void OnSubscribeToShortlivedObjects(object sender, EventArgs e)
+        {
+            int count = 10000;
+            for (int n = 0; n < count; n++)
+            {
+                var shortlived = new ShortLivedEventRaiser();
+                shortlived.OnSomething += ShortlivedOnOnSomething;
+            }
+            shortlivedEventRaiserCreated += count;
+        }
+
+        private void ShortlivedOnOnSomething(object sender, EventArgs eventArgs)
+        {
+            // just to prove that there is no smoke and mirrors, our event handler will do something involving the form
+            Text = "Got an event from a short-lived event raiser";
+        }
+
+        private void OnTimerTick(object sender, EventArgs e)
+        {
+            labelShortLived.Text = String.Format("{0} created, {1} still alive", shortlivedEventRaiserCreated, ShortLivedEventRaiser.Count);
+            labelShortLivedEventSubscribers.Text = String.Format("{0} created, {1} still alive", shortlivedEventSubscriberCreated, ShortLivedEventSubscriber.Count);
+            labelShortLivedPublishers.Text = String.Format("{0} created, {1} still alive", shortlivedEventPublisherCreated, ShortLivedEventPublisher.Count);
+            labelShortLivedSubscribers.Text = String.Format("{0} created, {1} still alive", shortlivedEventBusSubscriberCreated, ShortLivedEventBusSubscriber.Count);
+            labelShortLivedWeakSubscribers.Text = String.Format("{0} created, {1} still alive", shortlivedWeakEventSubscriberCreated, ShortLivedWeakEventSubscriber.Count);
+        }
+
+        private void OnShortlivedEventSubscribersClick(object sender, EventArgs e)
+        {
+            int count = 10000;
+            for (int n = 0; n < count; n++)
+            {
+                var shortlived2 = new ShortLivedEventSubscriber(this);
+            }
+            shortlivedEventSubscriberCreated += count;
+        }
+
+        private void OnForceGcClick(object sender, EventArgs e)
+        {
+            GC.Collect();
+        }
+
+        private void OnShortlivedEventPublishersClick(object sender, EventArgs e)
+        {
+            int count = 10000;
+            for (int n = 0; n < count; n++)
+            {
+                var shortlived3 = new ShortLivedEventPublisher(publisher1);
+            }
+            shortlivedEventPublisherCreated += count;
+        }
+
+        private void OnShortLivedEventBusSubscribersClick(object sender, EventArgs e)
+        {
+            int count = 10000;
+            for (int n = 0; n < count; n++)
+            {
+                var shortlived4 = new ShortLivedEventBusSubscriber(publisher2);
+            }
+            shortlivedEventBusSubscriberCreated += count;
+        }
+
+        private void OnShortLivedWeakSubscribersClick(object sender, EventArgs e)
+        {
+            int count = 10000;
+            for (int n = 0; n < count; n++)
+            {
+                var shortlived5 = new ShortLivedWeakEventSubscriber(weakEventAggregator);
+            }
+            shortlivedWeakEventSubscriberCreated += count;
+        }
+
+    }
+
+    public class ShortLivedEventRaiser
+    {
+        public static int Count;
+        
+        public event EventHandler OnSomething;
+
+        public ShortLivedEventRaiser()
+        {
+            Interlocked.Increment(ref Count);
+        }
+
+        protected void RaiseOnSomething(EventArgs e)
+        {
+            EventHandler handler = OnSomething;
+            if (handler != null) handler(this, e);
+        }
+
+        ~ShortLivedEventRaiser()
+        {
+            Interlocked.Decrement(ref Count);
+        }
+    }
+
+    public class ShortLivedEventSubscriber
+    {
+        public static int Count;
+
+        public string LatestText { get; private set; }
+
+        public ShortLivedEventSubscriber(Control c)
+        {
+            Interlocked.Increment(ref Count);
+            c.TextChanged += OnTextChanged;
+        }
+
+        private void OnTextChanged(object sender, EventArgs eventArgs)
+        {
+            LatestText = ((Control) sender).Text;
+        }
+
+        ~ShortLivedEventSubscriber()
+        {
+            Interlocked.Decrement(ref Count);
+        }
+    }
+
+
+    public class ShortLivedEventPublisher
+    {
+        public static int Count;
+        private readonly IEventPublisher publisher;
+
+        public ShortLivedEventPublisher(IEventPublisher publisher)
+        {
+            this.publisher = publisher;
+            Interlocked.Increment(ref Count);
+        }
+
+        public void PublishSomething()
+        {
+            publisher.Publish("Hello world");
+        }
+
+        ~ShortLivedEventPublisher()
+        {
+            Interlocked.Decrement(ref Count);
+        }
+    }
+
+    public class ShortLivedEventBusSubscriber
+    {
+        public static int Count;
+        public string LatestMessage { get; private set; }
+
+        public ShortLivedEventBusSubscriber(IEventPublisher publisher)
+        {
+            Interlocked.Increment(ref Count);
+            publisher.GetEvent<string>().Subscribe(s => LatestMessage = s);
+        }
+
+        ~ShortLivedEventBusSubscriber()
+        {
+            Interlocked.Decrement(ref Count);
+        }
+    }
+
+
+    public class ShortLivedWeakEventSubscriber
+    {
+        public static int Count;
+        public string LatestMessage { get; private set; }
+
+        public ShortLivedWeakEventSubscriber(WeakEventAggregator weakEventAggregator)
+        {
+            Interlocked.Increment(ref Count);
+            weakEventAggregator.Subscribe<string>(OnMessageReceived);
+        }
+
+        private void OnMessageReceived(string s)
+        {
+            LatestMessage = s;
+        }
+
+        ~ShortLivedWeakEventSubscriber()
+        {
+            Interlocked.Decrement(ref Count);
+        }
+    }
+
+    public interface IEventPublisher
+    {
+        void Publish<TEvent>(TEvent sampleEvent);
+        IObservable<TEvent> GetEvent<TEvent>();
+    }
+
+    public class EventPublisher : IEventPublisher
+    {
+        private readonly ConcurrentDictionary<Type, object> subjects
+            = new ConcurrentDictionary<Type, object>();
+
+        public IObservable<TEvent> GetEvent<TEvent>()
+        {
+            var subject =
+                (ISubject<TEvent>)subjects.GetOrAdd(typeof(TEvent),
+                            t => new Subject<TEvent>());
+            return subject.AsObservable();
+        }
+
+        public void Publish<TEvent>(TEvent sampleEvent)
+        {
+            object subject;
+            if (subjects.TryGetValue(typeof(TEvent), out subject))
+            {
+                ((ISubject<TEvent>)subject)
+                    .OnNext(sampleEvent);
+            }
+        }
+    }
+
+    public class WeakEventAggregator
+    {
+        class WeakAction
+        {
+            private WeakReference weakReference;
+            public WeakAction(object action)
+            {
+                weakReference = new WeakReference(action);
+            }
+
+            public bool IsAlive
+            {
+                get { return weakReference.IsAlive; }
+            }
+
+            public void Execute<TEvent>(TEvent param)
+            {
+                var action = (Action<TEvent>) weakReference.Target;
+                action.Invoke(param);
+            }
+        }
+
+        private readonly ConcurrentDictionary<Type, List<WeakAction>> subscriptions
+            = new ConcurrentDictionary<Type, List<WeakAction>>();
+
+        public void Subscribe<TEvent>(Action<TEvent> action)
+        {
+            var subscribers = subscriptions.GetOrAdd(typeof (TEvent), t => new List<WeakAction>());
+            subscribers.Add(new WeakAction(action));
+
+        }
+
+        public void Publish<TEvent>(TEvent sampleEvent)
+        {
+            List<WeakAction> subscribers;
+            if (subscriptions.TryGetValue(typeof(TEvent), out subscribers))
+            {
+                subscribers.RemoveAll(x => !x.IsAlive);
+                subscribers.ForEach(x => x.Execute<TEvent>(sampleEvent));
+            }
+        }
+    }
+
+    [TestClass]
+    public class WeakAggregatorTests
+    {
+
+        [TestMethod]
+        public void TestWeakAggregator()
+        {
+            string message1 = "x";
+            string message2 = "y";
+            string message3 = "z";
+            
+            var weakAggregator = new WeakEventAggregator();
+            weakAggregator.Subscribe<string>(s => message1 = "Subscriber 1 got " + s);
+            weakAggregator.Subscribe<string>(s => message2 = "Subscriber 2 got " + s);
+            weakAggregator.Subscribe<DateTime>(s => message3 = "Subscriber 3 got called");
+            weakAggregator.Publish("hello world");
+            Assert.AreEqual("Subscriber 1 got hello world", message1);
+            Assert.AreEqual("Subscriber 2 got hello world", message2);
+            Assert.AreEqual("z", message3);
+        }
+    }
+    
+}
